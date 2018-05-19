87fff46beb1d053b473faf8a8bac1d0ee4f5a5f4
Unknown <no1expman@gmail.com>
2018-05-20 02:46:27 +0900

    Fix TypeError from the use of imports.parchment of undefined Quill

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
                     loader: 'babel-loader',
                     options: {
                         "presets": [["es2015", { "modules": false }]],
-                        "plugins": ["babel-plugin-transform-class-properties"]
+                        "plugins": ["transform-class-properties"]
                     }
                 }]
             },
